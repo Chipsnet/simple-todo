4ce865dbb5d10bba6e62ae0374aeb3dae27e3d19
Minato Minami <doronomori660@outlook.jp>
2021-05-14 14:02:10 +0900

    Updates

---
+++ _nuxt/static/1620968529/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsimple-todo\u002F_nuxt\u002Fstatic\u002F1620968529",layout:"default",error:a,state:{tasks:[]},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsimple-todo\u002F",assetsPath:"\u002Fsimple-todo\u002F_nuxt\u002F",cdnURL:a}}}}(null));
